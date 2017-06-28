3f1bd0462dbfbfed745969629d6d41728a290845
Grezel Michal-XKTR67 <XKTR67@motorolasolutions.com>
2017-06-28 11:48:57 +0200

    logging fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ winston.warn('text from heroku: ' + process.env.TEST_ENV);
 // Setup logger
 app.use(bodyParser.json());
 let ignoreRoute = function (req, res) {
-    return blackList.indexOf(req.originalUrl || req.url) !== -1;
+    return blackList.indexOf(req.originalUrl || req.url) !== -1 || req.originalUrl.includes('/static/');
 };
 app.use(expressWinston.logger({
     transports: [
@@ -35,10 +35,12 @@ app.use(expressWinston.logger({
             colorize: true
         })
     ],
-    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    meta: false, // optional: control whether you want to log the meta data about the request (default to true)
     msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
     expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
     colorize: true, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    requestWhitelist:['url', 'headers', 'method', 'httpVersion'],
+    bodyBlacklist:["refresh_token","access_token"],
     ignoreRoute: ignoreRoute, // optional: allows to skip some log messages based on request and/or response
     skip: ignoreRoute
 }));
