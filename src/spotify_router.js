98c96b4b26ce564d6d72ca797fa0c00252817102
Michal Grezel <michal.grezel@grandparade.co.uk>
2018-02-22 16:41:32 +0100

    obtaining access token

--- src/spotify_router.js
+++ src/spotify_router.js
@@ -76,7 +76,7 @@ module.exports = function SpotifyHandlers() {
     } else {
       res.clearCookie(cookies_name.stateKey);
       const domain = url.parse(headers.referer).hostname;
-      winston.log('domain:', domain);
+      winston.info('domain:', domain);
       const spotifyApi = new Spotify(credentials);
       // Retrieve an access token and a refresh token
       spotifyApi
