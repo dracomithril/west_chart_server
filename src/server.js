0afcc2b87813ad34c422841aa408957b3c419166
Michal Grezel <michal.grezel@grandparade.co.uk>
2018-03-12 19:59:39 +0100

    typo

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ const { client } = config;
 module.exports = () => {
   const app = express();
 
-  app.use(cors({ origin: `https://${client}.herokuapi.com` }));
+  app.use(cors({ origin: `https://${client}.herokuapp.com` }));
   app.use(serveStatic(path.join(__dirname, '..', 'public')));
   app.set('views', path.join(__dirname, '..', 'public'));
   app.engine('html', ejs.renderFile);
